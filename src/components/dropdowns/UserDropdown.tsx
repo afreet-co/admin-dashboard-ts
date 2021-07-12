e3fd2bc3adc4f353b5a3bfd328fd4e9e1cb619d4
Afreet Co <afreet.co20@gmail.com>
2021-07-12 11:35:02 +0530

    Fixed: No values at config strings

--- src/components/dropdowns/UserDropdown.tsx
+++ src/components/dropdowns/UserDropdown.tsx
@@ -34,7 +34,7 @@ export const UserDropdown = () => {
           <span
             className={`w-8 h-8 text-sm text-dark-700 dark:text-light-400 border-2 uppercase border-light-200 dark:border-dark-600 bg-light-400 dark:bg-dark-800 inline-flex items-center justify-center rounded-full hover:bg-light-300 dark:hover:bg-dark-600`}
           >
-            {site_name.slice(0, 2)}
+            {site_name?.slice(0, 2)}
           </span>
         </div>
       </a>
--- src/configs/strings.ts
+++ src/configs/strings.ts
@@ -1,5 +1,5 @@
-export const site_name = process.env.NEXT_PUBLIC_APP_NAME;
-export const company_name = process.env.NEXT_PUBLIC_COMPANY_NAME;
-export const title = process.env.NEXT_PUBLIC_APP_NAME;
-export const applicationDescription = process.env.NEXT_PUBLIC_DESCRIPTION;
-export const locale = "en";
+export const site_name = process.env.NEXT_PUBLIC_APP_NAME || 'Admin-dashboard';
+export const company_name = process.env.NEXT_PUBLIC_COMPANY_NAME || 'Afreet-Co';
+export const title = process.env.NEXT_PUBLIC_APP_NAME  || 'Admin-dashboard';
+export const applicationDescription = process.env.NEXT_PUBLIC_DESCRIPTION  || '';
+export const locale = 'en';
