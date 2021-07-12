c9a37845c12d90e2e519f452bd3fbf94c9e04429
Afreet Co <afreet.co20@gmail.com>
2021-07-12 12:08:54 +0530

    FIxed: no favicon at deployment

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,15 @@ import { AppProps } from "next/app";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 import "../styles/main.css";
+import Head from "next/head";
 
 const Application = ({ Component, pageProps }: AppProps) => (
+  <>
+  <Head>
+    <link rel="shortcut icon" href={`${process.env.NEXT_PUBLIC_SITE_PREFIX}/favicon.ico`} type="image/x-icon" />
+  </Head>
   <Component {...pageProps} />
+  </>
 );
 
 export default Application;
