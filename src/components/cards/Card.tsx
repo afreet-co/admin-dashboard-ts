160b2968220a01af291df6e771d325a5501ac0f1
Afreet Co <afreet.co20@gmail.com>
2021-08-15 12:54:02 +0530

    Added: Size feature for card component

--- src/components/cards/Card.tsx
+++ src/components/cards/Card.tsx
@@ -1,10 +1,23 @@
 import { FC } from "react";
 import { classes } from "../../configs/classnames";
 
-export const Card: FC = ({ children }) => {
+type CardSizes = "full" | "half" | "third";
+
+interface CardProps {
+  width?: CardSizes;
+}
+const widthClasses: { [key in CardSizes]: string } = {
+  full: "w-full",
+  half: "w-1/2",
+  third: "w-1/3",
+};
+
+export const Card: FC<CardProps> = ({ width, children }) => {
   return (
     <div
-      className={`flex flex-col w-full shadow-lg rounded ${classes.background.dark}`}
+      className={`flex flex-col ${
+        widthClasses[width ?? "full"]
+      } shadow-lg rounded ${classes.background.dark} p-4`}
     >
       <div className="block w-full">{children}</div>
     </div>
