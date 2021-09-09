de31c3a309b2a0f305338ccc1453f64dd9b3740c
Afreet Co <afreet.co20@gmail.com>
2021-09-10 02:29:05 +0530

    Refactor: Removed repeated table code

--- src/components/tables/PaginatedTable.tsx
+++ src/components/tables/PaginatedTable.tsx
@@ -3,6 +3,7 @@ import { classes } from "../../configs/classnames";
 import { usePagination } from "../../hooks/usePagination";
 import { Button } from "../buttons/Button";
 import { Select } from "../input/Select";
+import { Table } from "./Table";
 
 interface PaginatedTableProps {
   columnNames: string[];
@@ -28,53 +29,14 @@ export const PaginatedTable: FC<PaginatedTableProps> = ({
     pageSize,
     onPageChange,
   } = usePagination(rows);
-
-  const tableHeadClasses = `align-middle border border-solid border-l-0 border-r-0 whitespace-nowrap font-semibold text-left ${classes.textBase} ${classes.border} p-2`;
-  const trClasses = `ease-linear transition-all duration-100 ${classes.background.hoverLight}`;
-  const tdClasses = `border-t-0 align-middle border-l-0 border-r-0 whitespace-nowrap p-2`;
-
   return (
-    <div>
-      <div
-        className={`flex flex-col min-w-0 break-words w-full p-4 h-96 overflow-auto  ${
-          boxShadow ? "shadow-lg" : ""
-        } rounded ${classes.background.base} ${classes.textBase}`}
-      >
-        <div className="block w-full">
-          <table className="table-auto items-center w-full bg-transparent border-collapse">
-            <thead>
-              <tr>
-                {columnNames.length > 0 &&
-                  columnNames.map((name) => (
-                    <th key={name} className={tableHeadClasses}>
-                      {name}
-                    </th>
-                  ))}
-                {options && <th className={tableHeadClasses}></th>}
-              </tr>
-            </thead>
-            <tbody>
-              {showableRows.length > 0 &&
-                showableRows.map((row, index) => (
-                  <tr key={index} className={trClasses}>
-                    {columnNames.map((columnName) => (
-                      <td key={columnName} className={tdClasses}>
-                        {row[columnName]}
-                      </td>
-                    ))}
-                    {options && (
-                      <td
-                        className={`${tdClasses} flex justify-end items-center w-full h-full`}
-                      >
-                        {options(row)}
-                      </td>
-                    )}
-                  </tr>
-                ))}
-            </tbody>
-          </table>
-        </div>
-      </div>
+    <>
+      <Table
+        columnNames={columnNames}
+        options={options}
+        rows={showableRows}
+        boxShadow={boxShadow}
+      />
       <div className={`${classes.background.base} flex mt-3 p-2 rounded`}>
         <div className={`flex flex-1 space-x-2 items-center`}>
           <Button onClick={onPrevClick} disabled={isPreviousDisabled}>
@@ -106,6 +68,6 @@ export const PaginatedTable: FC<PaginatedTableProps> = ({
           selected={pageSize}
         />
       </div>
-    </div>
+    </>
   );
 };
--- src/components/tables/Table.tsx
+++ src/components/tables/Table.tsx
@@ -18,47 +18,45 @@ export const Table: FC<TableProps> = ({
   const tdClasses = `border-t-0 align-middle border-l-0 border-r-0 whitespace-nowrap p-2`;
 
   return (
-    <>
-      <div
-        className={`flex flex-col min-w-0 break-words w-full p-4 h-96 overflow-auto  ${
-          boxShadow ? "shadow-lg" : ""
-        } rounded ${classes.background.base} ${classes.textBase}`}
-      >
-        <div className="block w-full">
-          <table className="table-auto items-center w-full bg-transparent border-collapse">
-            <thead>
-              <tr>
-                {columnNames.length > 0 &&
-                  columnNames.map((name) => (
-                    <th key={name} className={tableHeadClasses}>
-                      {name}
-                    </th>
-                  ))}
-                {options && <th className={tableHeadClasses}></th>}
-              </tr>
-            </thead>
-            <tbody>
-              {rows.length > 0 &&
-                rows.map((row, index) => (
-                  <tr key={index} className={trClasses}>
-                    {columnNames.map((columnName) => (
-                      <td key={columnName} className={tdClasses}>
-                        {row[columnName]}
-                      </td>
-                    ))}
-                    {options && (
-                      <td
-                        className={`${tdClasses} flex justify-end items-center w-full h-full`}
-                      >
-                        {options(row)}
-                      </td>
-                    )}
-                  </tr>
+    <div
+      className={`flex flex-col min-w-0 break-words w-full p-4 h-96 overflow-auto  ${
+        boxShadow ? "shadow-lg" : ""
+      } rounded ${classes.background.base} ${classes.textBase}`}
+    >
+      <div className="block w-full">
+        <table className="table-auto items-center w-full bg-transparent border-collapse">
+          <thead>
+            <tr>
+              {columnNames.length > 0 &&
+                columnNames.map((name) => (
+                  <th key={name} className={tableHeadClasses}>
+                    {name}
+                  </th>
                 ))}
-            </tbody>
-          </table>
-        </div>
+              {options && <th className={tableHeadClasses}></th>}
+            </tr>
+          </thead>
+          <tbody>
+            {rows.length > 0 &&
+              rows.map((row, index) => (
+                <tr key={index} className={trClasses}>
+                  {columnNames.map((columnName) => (
+                    <td key={columnName} className={tdClasses}>
+                      {row[columnName]}
+                    </td>
+                  ))}
+                  {options && (
+                    <td
+                      className={`${tdClasses} flex justify-end items-center w-full h-full`}
+                    >
+                      {options(row)}
+                    </td>
+                  )}
+                </tr>
+              ))}
+          </tbody>
+        </table>
       </div>
-    </>
+    </div>
   );
 };
